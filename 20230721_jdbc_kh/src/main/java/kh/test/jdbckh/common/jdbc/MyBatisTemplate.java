701016acaa43e5a227eb827f99bb5e9d0fe48aef
LIMJIYEON59 <chayi2110@gmail.com>
2023-08-17 15:10:49 +0900

    mybatis

---
+++ 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/common/jdbc/MyBatisTemplate.java
@@ -0,0 +1,23 @@
+package kh.test.jdbckh.common.jdbc;
+
+import java.io.IOException;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MyBatisTemplate {
+	public static SqlSession getSqlSession() {
+		SqlSession session = null;
+		
+		try {
+			SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+		return session;
+	}
+
+}
